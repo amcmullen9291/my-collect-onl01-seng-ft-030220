f8568a362769eb30c6c5edcd2c685e16dc38949e
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-05 15:11:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,2 @@
-def my_collection 
-my_each(array)
-i = 0
-  while i < array.length
-    yield
-    (array[i])do || my_collect<< {}
-    i += 1
-  end
-return my_collection
-
-
-
+def my_collect(names)
+  end)
